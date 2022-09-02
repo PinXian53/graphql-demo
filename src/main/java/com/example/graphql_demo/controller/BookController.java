6509d725c239a1c60f14b20d11847838a9f0aaa6
Pin <pino.chang@tpisoftware.com>
2022-09-02 23:06:30 +0800

    feat: create graphql api and set test data

---
+++ src/main/java/com/example/graphql_demo/controller/BookController.java
@@ -0,0 +1,21 @@
+package com.example.graphql_demo.controller;
+
+import com.example.graphql_demo.dao.Author;
+import com.example.graphql_demo.dao.Book;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookController {
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.getAuthorId());
+    }
+}
